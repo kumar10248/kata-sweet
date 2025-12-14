2bf260340f2944a1d191f6c435cf0c105363e9ab
kumar10248 <kumardevashish000@gmail.com>
2025-12-14 22:11:04 +0530

    fix: Update API base URL and adjust build script in package.json; add render.yaml for deployment configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // backend base URL
+  baseURL: "https://kata-sweet.onrender.com/api", // backend base URL
   headers: {
     "Content-Type": "application/json"
   }
