7f6eae6afdfcee0c47b1afd9667372746a070962
kumar10248 <kumardevashish000@gmail.com>
2025-12-14 16:15:14 +0530

    chore: frontend renders App successfully

--- incube-frontend/src/App.jsx
+++ incube-frontend/src/App.jsx
@@ -1,22 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Admin from "./pages/Admin";
-import Navbar from "./components/Navbar";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>APP IS RENDERING</h1>;
 }
 
 export default App;
